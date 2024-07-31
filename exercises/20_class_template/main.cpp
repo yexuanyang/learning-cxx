c85f20b86e8c8a81f250043ac65e5af4da4d6f1d
yexuanyang <1182282462@qq.com>
2024-07-31 15:40:29 +0800

    pass all cpp

--- exercises/20_class_template/main.cpp
+++ exercises/20_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; i++) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,22 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        unsigned int stride[4] = {shape[1] * shape[2] * shape[3], shape[2] * shape[3], shape[3], 1};
+        unsigned int other_stride[4] = {others.shape[1] * others.shape[2] * others.shape[3], others.shape[2] * others.shape[3], others.shape[3], 1};
+        for(int i = 0; i < shape[0]; i ++){
+            for(int j = 0; j < shape[1]; j ++){
+                for (int k = 0; k < shape[2]; k ++){
+                    for(int l = 0; l < shape[3]; l ++){
+                        int ox = others.shape[0] == 1 ? 0 : i;
+                        int oy = others.shape[1] == 1 ? 0 : j;
+                        int oz = others.shape[2] == 1 ? 0 : k;
+                        int ow = others.shape[3] == 1 ? 0 : l;
+                        data[i * stride[0] + j * stride[1] + k * stride[2] + l * stride[3]] += 
+                        others.data[ox * other_stride[0] + oy * other_stride[1] + oz * other_stride[2] + ow * other_stride[3]];
+                    }
+                }
+            }
+        }
         return *this;
     }
 };
--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -32,9 +36,17 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
+        unsigned int stride[N];
+        for (unsigned int i = 0; i < N; ++i) {
+            stride[i] = 1;
+            for (unsigned int j = i + 1; j < N; ++j) {
+                stride[i] *= shape[j];
+            }
+        }
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            index += indices[i] * stride[i];
         }
         return index;
     }
